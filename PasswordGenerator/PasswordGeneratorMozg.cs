5ef2a6a120dcc79488b68e281f11f413f90f3865
Blossantrape <tolik.hamer@gmail.com>
2022-07-07 02:31:24 +0300

    Добавьте файлы проекта.

---
+++ PasswordGenerator/PasswordGeneratorMozg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PasswordGeneratorMozg
+{
+    class Mozg
+    {
+        static void Main(string[] args)
+        {
+            // Дописать все возможные варианты
+            Console.WriteLine("Сколько символов в пароле?");
+            Console.ReadLine();
+
+            Console.WriteLine("Использовать двойной регистр символом?");
+            Console.ReadLine();
+
+            Console.WriteLine("Сколько символов в пароле?");
+            Console.ReadLine();
+
+            // Сымволы для пароля
+            string abc = "abcdefghijklmnopqrstuwxyz";
+            string numbers = "0123456789";
+            string symbols = "!@#$%^&*()";
+
+            // Тут где-то логика будет
+
+
+        }
+          
+    }
+}
